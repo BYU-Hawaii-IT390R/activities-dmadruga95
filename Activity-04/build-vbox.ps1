f70ee010c4187f71f7e4e58f0f4ecc0640b35b3a
dmadruga95 <163998377+dmadruga95@users.noreply.github.com>
2025-06-26 00:19:18 -1000

    Add files via upload

--- Activity-04/build-vbox.ps1
+++ Activity-04/build-vbox.ps1
@@ -1,10 +1,32 @@
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" createvm --name "AutomatedWin10" --register
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" modifyvm "AutomatedWin10" --memory 4096 --cpus 2 --ostype "Windows10_64"
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" createmedium disk --filename "C:\ISO Folder\AutomatedWin10.vdi" --size 40000
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storagectl "AutomatedWin10" --name "SATA Controller" --add sata --controller IntelAhci
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "C:\ISO Folder\AutomatedWin10.vdi"
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storagectl "AutomatedWin10" --name "IDE Controller" --add ide
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium "C:\ISO Folder\en-us_windows_10_consumer_editions_version_22h2_x64_dvd_8da72ab3.iso"
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" storageattach "AutomatedWin10" --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium "C:\ISO Folder\answer.iso"
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" modifyvm "AutomatedWin10" --nic1 nat
-& "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe" startvm "AutomatedWin10"
+# PowerShell script to create a Windows 10 VM using VirtualBox
+
+# Paths
+$vmName     = "AutomatedWin10"
+$isoFolder  = "C:\ISO Folder"
+$winISO     = "$isoFolder\en-us_windows_10_consumer_editions_version_22h2_x64_dvd_8da72ab3.iso"
+$answerISO  = "$isoFolder\answer.iso"
+$vdiPath    = "$isoFolder\AutomatedWin10.vdi"
+
+# VBoxManage location (adjust if VirtualBox is installed somewhere else)
+$vboxManage = "C:\Program Files\Oracle\VirtualBox\VBoxManage.exe"
+
+# Create VM
+& "$vboxManage" createvm --name $vmName --register
+& "$vboxManage" modifyvm $vmName --memory 4096 --cpus 2 --ostype "Windows10_64"
+
+# Create virtual hard disk
+& "$vboxManage" createmedium disk --filename $vdiPath --size 40000
+
+# Add storage controllers
+& "$vboxManage" storagectl $vmName --name "SATA Controller" --add sata --controller IntelAhci
+& "$vboxManage" storageattach $vmName --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium $vdiPath
+
+& "$vboxManage" storagectl $vmName --name "IDE Controller" --add ide
+& "$vboxManage" storageattach $vmName --storagectl "IDE Controller" --port 0 --device 0 --type dvddrive --medium $winISO
+& "$vboxManage" storageattach $vmName --storagectl "IDE Controller" --port 1 --device 0 --type dvddrive --medium $answerISO
+
+# Enable network
+& "$vboxManage" modifyvm $vmName --nic1 nat
+
+# Start VM
+& "$vboxManage" startvm $vmName
